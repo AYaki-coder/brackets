531425305a1169ddd99328bc88ae68d3d6636b2a
ayaki-coder <sj.a.yakimovich@gmail.com>
2020-03-19 00:32:22 +0300

    feat: add my solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+  let condition = [];
+  for (let i = 0; i < bracketsConfig.length; i++) {
+
+    condition[i] = bracketsConfig[i].join('');
+
+  }
+  let foundIT = true;
+  for (; foundIT;) {
+    let j = 0;
+    for (let i = 0; i < condition.length; i++) {
+
+      let pos = str.indexOf(condition[i]);
+      j = j + pos;
+      foundIT = j == -1 * (condition.length) ? false : true;
+      if (pos > -1) {
+        str = str.slice(0, pos) + str.slice(pos + 2);
+
+      }
+    }
+  }
+  return str.length > 0 ? false : true;
+
 }
